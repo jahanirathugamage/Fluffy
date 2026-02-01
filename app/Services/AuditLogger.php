756b93b098bf266236bb29e609c7b67bee9006e7
jahanirathugamage <jahanigaveshii@gmail.com>
2026-02-02 00:54:10 +0530

    Deployment

--- app/Services/AuditLogger.php
+++ app/Services/AuditLogger.php
@@ -2,18 +2,12 @@
 
 namespace App\Services;
 
-use MongoDB\Client;
+use Illuminate\Support\Facades\Log;
 
 class AuditLogger
 {
     public static function log(array $data): void
     {
-        $client = new Client("mongodb://127.0.0.1:27017");
-        $collection = $client->fluffy_logs->audit_logs;
-
-        $collection->insertOne([
-            ...$data,
-            'created_at' => now()
-        ]);
+        Log::channel('stderr')->info('Audit Log:', $data);
     }
 }
